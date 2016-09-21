569b4688591ad496f6bfe7e0d2198b51238137cb
Kinderley Charles <kinderleycharles@aol.com>
2016-09-20 23:34:26 -0400

    Sign In page is set as landing page

--- kai/modules/core/client/controllers/home.client.controller.js
+++ kai/modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,20 @@
     .module('core')
     .controller('HomeController', HomeController);
 
-  function HomeController() {
+  HomeController.$inject = ['UsersService', '$location', 'Authentication'];
+
+  function HomeController(UsersService, $location, Authentication) {
     var vm = this;
+
+    vm.authentication = Authentication;
+
+    // If user is signed in then redirect back home
+    if (vm.authentication.user) {
+      $location.path('/');
+    } else {
+      // Redirect to Sign in page
+      // i.e. this makes the Sign in Page the landing page
+      $location.path('/signin?err');
+    }
   }
 }());
