9e0c5a4ba855f1fd090889e296aee19e5d0c8d79
Kinderley Charles <kinderleycharles@aol.com>
2016-11-07 22:53:37 -0500

    Listing devices design done

--- kai/modules/devices/client/controllers/list-devices.client.controller.js
+++ kai/modules/devices/client/controllers/list-devices.client.controller.js
@@ -10,6 +10,39 @@
   function DevicesListController(DevicesService) {
     var vm = this;
 
-    vm.devices = DevicesService.query();
+    vm.devices = [{
+      _id: 0,
+      _devid: 'light',
+      _name: 'Light',
+      _des: 'This is the light in the living room',
+      _devType: 1,
+      _state: 1,
+      _status: 1
+    }, {
+      _id: 1,
+      _devid: 'door',
+      _name: 'Door',
+      _des: 'This is the front door of the house',
+      _devType: 1,
+      _state: 1,
+      _status: 1
+    }, {
+      _id: 2,
+      _devid: 'coffee',
+      _name: 'Coffee Machine',
+      _des: 'Coffee machine in my kitchen',
+      _devType: 1,
+      _state: 1,
+      _status: 1
+    }, {
+      _id: 3,
+      _devid: 'wind',
+      _name: 'Windows',
+      _des: 'Living room windows',
+      _devType: 1,
+      _state: 1,
+      _status: 1
+    }];
+    // vm.devices = DevicesService.query();
   }
 }());
